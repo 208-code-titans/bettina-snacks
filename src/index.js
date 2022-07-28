686218a84af08b0e5c49f5bb2ed58918f231fe37
Nutifafa Attor <attorfafa@gmail.com>
2022-07-28 19:45:19 +0000

    feat: :sparkles: wrapped app in router

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { BrowserRouter as Router } from 'react-router-dom'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+	<Router>
+		<App />
+	</Router>
+)
