40d64861efa3b2804b1315114d77ad7c5fbc6593
Nutifafa Attor <attorfafa@gmail.com>
2022-07-28 19:44:51 +0000

    feat: :sparkles: set up routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import { HomePage } from './pages/pages'
+import {
+	HomePage,
+	AboutPage,
+	ContactPage,
+	DashboardPage,
+	MenuPage,
+	SignInPage,
+	SignUpPage,
+} from './pages/pages'
+import { Routes, Route } from 'react-router-dom'
+import {
+	HOME,
+	ABOUT,
+	MENU,
+	CONTACT,
+	SIGN_UP,
+	SIGN_IN,
+} from './constants/routes'
+import { AnimatePresence } from 'framer-motion'
+import {Header} from './components/components'
 
 function App() {
-  return (
-    <div className="">
-      <HomePage />
-    </div>
-  );
+	return (
+    <AnimatePresence exitBeforeEnter>
+      <Header />
+      
+			<Routes>
+				<Route path={HOME} element={<HomePage />} />
+				<Route path={ABOUT} element={<AboutPage />} />
+				<Route path={MENU} element={<MenuPage />} />
+				<Route path={CONTACT} element={<ContactPage />} />
+				<Route path={SIGN_IN} element={<SignInPage />} />
+				<Route path={SIGN_UP} element={<SignUpPage />} />
+			</Routes>
+		</AnimatePresence>
+	)
 }
 
-export default App;
+export default App
