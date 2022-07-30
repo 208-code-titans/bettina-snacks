426998c6f848e834393dea349844358bf52e405d
Nutifafa Attor <attorfafa@gmail.com>
2022-07-30 11:22:21 +0000

    feat: :sparkles: responsive home landing page

--- src/components/HomeLanding/HomeLanding.jsx
+++ src/components/HomeLanding/HomeLanding.jsx
@@ -15,10 +15,22 @@ const HomeLanding = () => {
 					<Button route='/menu' text='Order Now' />
 				</div>
 				<div className='absolute right-0 w-[500px] 2xl:w-[600px]'>
-					<img src={homeLanding} alt='' className='w-[] ' />
+					<img src={homeLanding} alt='' className='' />
 				</div>
 			</div>
-			<div className='flex md:hidden w-full h-full justify-between items-center'></div>
+			<div className='flex md:hidden w-full h-full justify-between items-center flex-col gap-4 px-8 pt-12'>
+				<div className='w-full relative mx-12 gap-4 flex flex-col'>
+					<LandingSubHeading
+						title='The Pastries With A'
+						span='Difference'
+						subtitle='Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque hic aperiam deserunt, sed consequuntur minima et, ex dicta corporis rem in eaque porro? Explicabo inventore repellendus sapiente asperiores delectus nihil.'
+					/>
+					<Button route='/menu' text='Order Now' />
+                </div>
+                <div className=' right-0 w-[300px]'>
+					<img src={homeLanding} alt='' className='rotate-90' />
+				</div>
+			</div>
 		</div>
 	)
 }
