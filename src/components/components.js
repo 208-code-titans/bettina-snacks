4a353037abac6c53821f6daf6573c4ebc94d4a46
Nutifafa Attor <attorfafa@gmail.com>
2022-07-30 10:55:30 +0000

    feat: :sparkles: update

--- src/components/components.js
+++ src/components/components.js
@@ -1,1 +1,6 @@
 export { default as Header} from './Header/Header'
+export { default as HomeLanding } from './HomeLanding/HomeLanding'
+
+
+export { default as Button} from './Button/Button'
+export { default as LandingSubHeading} from './LandingSubHeading/LandingSubHeading'
