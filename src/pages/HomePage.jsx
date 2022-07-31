91b9b0016c444a69a14c95ab0e40d37e2f65a323
Nutifafa Attor <attorfafa@gmail.com>
2022-07-31 20:59:26 +0000

    feat: :sparkles: structured home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import { HomeLanding } from '../components/components'
+import { HomeLanding, HomeAbout, HomeContact, HomeExplore, HomeTestimonials } from '../components/components'
 
 const HomePage = () => {
   return (
     <div className='pt-[67px] md:pt[67px] w-screen h-screen'>
       <HomeLanding />
+      <HomeAbout />
+      <HomeExplore />
+      <HomeTestimonials />
+      <HomeContact />
+
     </div>
   )
 }
