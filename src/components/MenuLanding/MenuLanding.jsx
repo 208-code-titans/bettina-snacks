f56d48046e81db6055c5348c68df1d088134036b
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-07 15:19:41 +0000

    feat: :sparkles: created menu landing page

---
+++ src/components/MenuLanding/MenuLanding.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { menuLanding } from '../images'
+
+const MenuLanding = () => {
+  return (
+      <div className='w-screen h-[70vh] bg-blue-400 bg-cover bg-[center_top_-10rem] bg-no-repeat  shadow-xl' style={{ backgroundImage: `url(${menuLanding})` } }>
+          
+    </div>
+  )
+}
+
+export default MenuLanding
