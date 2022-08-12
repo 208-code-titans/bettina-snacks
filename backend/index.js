6a56d87e6e34086d141ceeb2fb0266595f5aea1a
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-12 07:36:44 +0000

    feat: :sparkles: connected to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,33 @@
-import express from "express";
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
 const app = express()
+dotenv.config()
+
+// Connect Mongoose and handle initial connection errors
+const connect = async () => {
+    try {
+        // removed await
+		mongoose.connect(process.env.MONGO)
+		console.log('Connected to MongoDB!!!')
+	} catch (error) {
+		throw error
+	}
+}
+
+// Listen to diconnection status
+mongoose.connection.on('disconnected', () => {
+	console.log('MongoDB disconnected')
+})
+
+// Listen to connection status
+mongoose.connection.on('connected', () => {
+	console.log('MongoDB connected')
+})
 
 // Run app on free port
 app.listen(8800, () => {
-    console.log("Connected to backend!!!!!!!!");
+	connect()
+	console.log('Connected to backend!!!!!!!!')
 })
