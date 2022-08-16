9156a4f0d736d2d334da0be7f9df59e192787cb6
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-16 09:41:35 +0000

    feat: :sparkles: update

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { DashboardAdd } from '../components/components'
+import { DashboardAdd, PendingOrders } from '../components/components'
 
 const DashboardPage = () => {
-  return (
-    <div className='pt-[67px] md:pt[67px] w-screen h-screen'>
-      <DashboardAdd />
-    </div>
-  )
+	return (
+		<div className='pt-[67px] md:pt[67px] w-screen h-screen'>
+			<div className='flex flex-col xl:flex-row justify-center'>
+				<DashboardAdd />
+				<PendingOrders />
+			</div>
+		</div>
+	)
 }
 
 export default DashboardPage
