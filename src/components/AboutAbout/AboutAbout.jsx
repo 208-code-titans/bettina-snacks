e75ca872be916e6f2fa87612a5538770c9829620
Khobi505 <kwabena.adutekyi505@outlook.com>
2022-08-20 00:59:04 +0000

    About Page Completed

---
+++ src/components/AboutAbout/AboutAbout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const AboutAbout = () => {
+  return (
+    <div className = 'text-center bg-zinc-300 pt-[40px] pb-[200px] border-none rounded-[50px]'>
+        <div className = ' mx-[200px]'>
+            <h3 className = 'text-2xl font-bold'>ABOUT</h3>
+                <p className = 'mt-[50px] '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                In ut vehicula erat. Nunc varius augue nisi, sed egestas mauris molestie at. 
+                Donec sed purus porta, auctor purus a, volutpat nulla. Pellentesque a finibus ligula, vitae sagittis nibh.
+                In fermentum, urna ac vulputate convallis, dolor dui ullamcorper lacus, non aliquam libero eros a dolor.
+                Mauris quis urna imperdiet, sagittis massa eget, malesuada neque. Vivamus non est nisi. 
+                Nullam congue odio vel accumsan rutrum. Ut lobortis quam et sem tristique accumsan. 
+                Nullam aliquet tincidunt libero et sollicitudin.</p>
+        </div>
+    </div>
+  )
+}
+
+export default AboutAbout
--- src/components/components.js
+++ src/components/components.js
@@ -15,6 +15,9 @@ export { default as HomeContact } from './HomeContact/HomeContact'
 
 // About Page
 export { default as AboutLanding } from './AboutLanding/AboutLanding'
+export {default as AboutHistory} from './AboutHistory/AboutHistory'
+export {default as AboutAbout } from './AboutAbout/AboutAbout'
+export {default as AboutEvent } from './AboutEvent/AboutEvent'
 
 // Menu Page
 export { default as MenuLanding } from './MenuLanding/MenuLanding'
