57ff4389211bd20631009b03a25a223b3e0fb0b1
Annette Theodore <annettetheodore582@gmail.com>
2022-08-22 21:13:46 +0000

    style: :lipstick: re-styled

--- src/components/AboutAbout/AboutAbout.jsx
+++ src/components/AboutAbout/AboutAbout.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
+import { aboutdeco2 } from "../images"
+
 
 const AboutAbout = () => {
   return (
-    <div className = 'text-center bg-zinc-300 pt-[40px] pb-[200px] border-none rounded-[50px]'>
-        <div className = ' mx-[200px]'>
-            <h3 className = 'text-2xl font-bold'>ABOUT</h3>
-                <p className = 'mt-[50px] '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                In ut vehicula erat. Nunc varius augue nisi, sed egestas mauris molestie at. 
-                Donec sed purus porta, auctor purus a, volutpat nulla. Pellentesque a finibus ligula, vitae sagittis nibh.
-                In fermentum, urna ac vulputate convallis, dolor dui ullamcorper lacus, non aliquam libero eros a dolor.
-                Mauris quis urna imperdiet, sagittis massa eget, malesuada neque. Vivamus non est nisi. 
-                Nullam congue odio vel accumsan rutrum. Ut lobortis quam et sem tristique accumsan. 
-                Nullam aliquet tincidunt libero et sollicitudin.</p>
-        </div>
+    <div className='text-center mb-8 border-none h-[80vh] flex items-center justify-center text-white bg-cover' style={{ backgroundImage: `url(${aboutdeco2})` }}>
+      <div className=' mx-auto flex flex-col gap-9'>
+        <h1 className='text-7xl uppercase'>ABO<span className='text-red-600'>UT</span></h1>
+
+        <p className='max-w-4xl text-center mx-auto '>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          In ut vehicula erat. Nunc varius augue nisi, sed egestas mauris molestie at.
+          Donec sed purus porta, auctor purus a, volutpat nulla. Pellentesque a finibus ligula, vitae sagittis nibh.
+          In fermentum, urna ac vulputate convallis, dolor dui ullamcorper lacus, non aliquam libero eros a dolor.
+          Mauris quis urna imperdiet, sagittis massa eget, malesuada neque. Vivamus non est nisi.
+          Nullam congue odio vel accumsan rutrum. Ut lobortis quam et sem tristique accumsan.
+          Nullam aliquet tincidunt libero et sollicitudin.
+        </p>
+      </div>
     </div>
   )
 }
