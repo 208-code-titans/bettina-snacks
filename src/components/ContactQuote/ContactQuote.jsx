01a9421ebaaccefc33c2aa1a2ff61a03b7e54ba5
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-26 11:28:14 +0000

    feat: :sparkles: update

--- src/components/ContactQuote/ContactQuote.jsx
+++ src/components/ContactQuote/ContactQuote.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { cookiesBg } from '../images'
 const ContactQuote = () => {
   return (
-    <div className='w-screen h-screen bg-center shadow-xl flex items-center justify-center bg-cover bg-gradient-to-br from-red-500 via-pink-700 to-red-500' >
-      <div className='text-2xl text-center text-white '>
-          <p>"It's all about a balancing act between time, temperature and <br /> ingredients: That's the art of baking" </p>
+    <div className='w-screen h-[70vh] bg-center shadow-xl flex items-center justify-center bg-cover bg-gradient-to-br from-red-500 via-pink-700 to-red-500 px-4' >
+      <div className='text-2xl text-center text-white w-full lg:max-w-2xl '>
+          <p>"It's all about a balancing act between time, temperature and  ingredients: That's the art of baking" </p>
 
       </div>
     </div>
