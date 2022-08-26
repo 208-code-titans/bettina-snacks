3096ce6fea682289a1f41c79839442d250afc8ae
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-26 11:35:47 +0000

    feat: :sparkles: called products from database

--- src/components/MenuContainer/MenuContainer.jsx
+++ src/components/MenuContainer/MenuContainer.jsx
@@ -1,14 +1,61 @@
-import React from 'react'
-import { LandingSubHeading } from '../components'
+import React, { useState, useEffect } from 'react'
+import { LandingSubHeading, Product, Loader } from '../components'
+import { onSnapshot, collection, query, orderBy } from '@firebase/firestore'
+import { firestore } from '../../firebase.config'
 
 const MenuContainer = () => {
-  return (
-      <div className='w-full py-12 px-20'>
-          <div>
-              <LandingSubHeading title={"Explore Our"} span={"Menu"} className='text-3xl' />
-          </div>
-      </div>
-  )
+	const [allProducts, setAllProducts] = useState([])
+	const [loading, setLoading] = useState(false)
+
+	useEffect(() => {
+		setLoading(true)
+		return onSnapshot(
+			query(collection(firestore, 'products')),
+			(snapshot) => {
+				setAllProducts(snapshot.docs)
+				setLoading(false)
+			}
+		)
+	}, [firestore])
+	// console.log(allProducts)
+
+	return (
+		<div className='w-full py-12 px-20'>
+			<div>
+				<LandingSubHeading
+					title={'Explore Our'}
+					span={'Menu'}
+					className='text-3xl'
+				/>
+			</div>
+			<div>
+				{loading ? (
+					<>
+						<div className='flex w-full items-center justify-center'>
+							<Loader />
+						</div>
+					</>
+				) : (
+					<>
+						<div className=' py-8 grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-10 w-full'>
+							{allProducts &&
+								allProducts.map((item) => (
+									<Product
+										key={item.id}
+										id={item.id}
+										img={item.data().photo}
+										name={item.data().name}
+										category={item.data().category}
+										price={item.data().price}
+										tag={item.data().tag}
+									/>
+								))}
+						</div>
+					</>
+				)}
+			</div>
+		</div>
+	)
 }
 
 export default MenuContainer
