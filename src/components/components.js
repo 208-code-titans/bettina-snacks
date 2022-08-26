a039f965a3f37b6e7a07a3655be1815f2e9abe39
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-26 11:27:50 +0000

    feat: :sparkles: update

--- src/components/components.js
+++ src/components/components.js
@@ -2,6 +2,7 @@ export { default as Header } from './Header/Header'
 export { default as Footer } from './Footer/Footer'
 export { default as Button} from './Button/Button'
 export { default as Loader} from './Loader/Loader'
+export { default as Product} from './Product/Product'
 export { default as LandingSubHeading } from './LandingSubHeading/LandingSubHeading'
 
 
