b3a321a4d79a42a7cb7d7bf03d1a2a579505adb3
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-27 12:49:23 +0000

    fix: :bug: responsiveness bugs

--- src/components/AboutLanding/AboutLanding.jsx
+++ src/components/AboutLanding/AboutLanding.jsx
@@ -5,11 +5,11 @@ import { LandingSubHeading } from '../components'
 const AboutLanding = () => {
 	return (
 		<div className='flex flex-col-reverse md:flex-row pt-12 h-screen '>
-			<div className=' w-full md:w-[60%]'>
-				<img src={aboutLanding} alt='' className='w-full object-cover' />
+			<div className=' w-full lg:w-[60%] md:h-full flex items-center justify-center'>
+				<img src={aboutLanding} alt='' className='w-full  object-cover' />
 			</div>
 
-			<div className='w-full md:w-[40%] p-8 flex items-center justify-center'>
+			<div className='w-full lg:w-[40%]  p-8 flex items-center justify-center'>
 				<LandingSubHeading
 					title={'About Bettina'}
 					span={'Snacks'}
