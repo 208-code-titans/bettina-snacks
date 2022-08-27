654c2b10575216e3f33cc5441855a8a0a98f13c0
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-27 10:42:43 +0000

    feat: :sparkles: store cart items in local storage

--- src/utils/fetchLocalStorageData.js
+++ src/utils/fetchLocalStorageData.js
@@ -7,3 +7,13 @@ export const fetchUser = () => {
     
     return userInfo
 }
+
+export const fetchCart = () => {
+	// store user info in cartInfo if user is not undefined
+	const cartInfo =
+		localStorage.getItem('cartItem') !== 'undefined'
+			? JSON.parse(localStorage.getItem("cartItems"))
+            : localStorage.clear()
+    
+    return cartInfo ? cartInfo : []
+}
