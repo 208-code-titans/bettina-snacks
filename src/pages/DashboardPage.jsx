717f678ea8461df667945baa124b9107f1796920
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-09-02 08:36:19 +0000

    refactor: :sparkles: update

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -4,10 +4,10 @@ import { DashboardAdd, PendingOrders } from '../components/components'
 const DashboardPage = () => {
 	return (
 		<div className='pt-[67px] md:pt[67px] w-screen h-screen'>
-			<div className='flex flex-col xl:flex-row justify-center'>
+			{/* <div className='flex flex-col xl:flex-row justify-center'> */}
 				<DashboardAdd />
 				<PendingOrders />
-			</div>
+			{/* </div> */}
 		</div>
 	)
 }
