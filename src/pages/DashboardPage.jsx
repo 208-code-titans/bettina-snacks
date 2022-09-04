8f1bfba92723cec74094be10dee6db4f8d00f74e
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-09-04 14:15:20 +0000

    feat: :sparkles: update

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import { DashboardAdd, PendingOrders, OngoingOrders, DeclinedOrders } from '../components/components'
+import { useStateValue } from '../context/StateProvider'
+import { HOME, SIGN_IN } from '../constants/routes'
+import { Navigate } from 'react-router-dom'
+
 
 const DashboardPage = () => {
+	// const [{ user }] = useStateValue()
+	// console.log(user)
+	// if (user) {
+	// 	if (user.email !== 'snacksbettina@gmail.com') {
+	// 		return <Navigate to={HOME} replace/>
+	// 	}
+	// 	return <Navigate to={SIGN_IN} replace />
+	// }
 	return (
 		<div className='pt-[67px] md:pt[67px] w-screen min-h-screen mb-12'>
 			<div className='flex flex-col xl:flex-row justify-center max-h-screen'>
