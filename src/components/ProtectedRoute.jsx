8261121f17edefd92ea16c1da71b3a8fa35635d2
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-09-04 14:14:19 +0000

    feat: :sparkles: security update

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+
+
+const ProtectedRoute = ({ user, children }) => {
+    if (!user) {
+      return <Navigate to="/signin" replace />;
+    }
+  
+    return children;
+  };
+export default ProtectedRoute
